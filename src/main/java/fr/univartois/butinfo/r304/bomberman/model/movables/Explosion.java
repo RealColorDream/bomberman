a55ca5478757b69e55c535f1feea8ad0bacb9181
A. Jakubiak <contact@jakubiak.fr>
2024-10-01 10:46:19 +0200

    :sparkles: :construction: #4 Created class

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Explosion.java
@@ -0,0 +1,36 @@
+package fr.univartois.butinfo.r304.bomberman.model.movables;
+
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+
+public class Explosion extends AbstractMovable {
+
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Explosion(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+        //TODO ?
+    }
+
+    @Override
+    public void collidedWith(IMovable other) {
+        //TODO
+    }
+
+    @Override
+    public void explode() {
+        //TODO
+    }
+
+    @Override
+    public void hitEnemy() {
+        //TODO
+    }
+}
