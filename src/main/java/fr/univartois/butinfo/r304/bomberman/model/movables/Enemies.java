0ade4f6933daea80e2492e54f7b8e18f13c6c512
loann <loann_mailly@ens.univ-artois.fr>
2024-10-04 11:07:18 +0200

    :sparkles: :construction: #18 Created Enemies class

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Enemies.java
@@ -0,0 +1,34 @@
+package fr.univartois.butinfo.r304.bomberman.model.movables;
+
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+
+public class Enemies extends AbstractMovable {
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Enemies(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
+
+    @Override
+    public void collidedWith(IMovable other) {
+        //TODO
+    }
+    @Override
+    public void explode() {
+        //TODO
+    }
+
+    @Override
+    public void hitEnemy() {
+        //TODO
+    }
+}
+
