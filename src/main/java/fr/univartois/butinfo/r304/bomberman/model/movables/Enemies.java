47583e5a46650a1c1e2306e3d97be66e0bc4650a
loann <loann_mailly@ens.univ-artois.fr>
2024-10-04 11:13:17 +0200

    :sparkles: :construction: #19 Redefined the move method

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Enemies.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Enemies.java
@@ -4,6 +4,8 @@ import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
+import java.util.Random;
+
 public class Enemies extends AbstractMovable {
     /**
      * Crée une nouvelle instance de AbstractMovable.
@@ -13,10 +15,40 @@ public class Enemies extends AbstractMovable {
      * @param yPosition La position en y initiale de l'objet.
      * @param sprite    L'instance de {@link Sprite} représentant l'objet.
      */
+    private static final Random random = new Random();
+    private boolean isAlive;
+
     protected Enemies(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
         super(game, xPosition, yPosition, sprite);
     }
 
+    public boolean move(int mouvement) {
+        if (!isAlive) {
+            return false;
+        }
+        int direction = random.nextInt(4);
+        switch (direction) {
+            case 0: // Haut
+                setVerticalSpeed(-1);
+                setHorizontalSpeed(0);
+                break;
+            case 1: // Bas
+                setVerticalSpeed(1);
+                setHorizontalSpeed(0);
+                break;
+            case 2: // Gauche
+                setVerticalSpeed(0);
+                setHorizontalSpeed(-1);
+                break;
+            case 3: // Droite
+                setVerticalSpeed(0);
+                setHorizontalSpeed(1);
+                break;
+            default:
+                break;
+        }
+        return super.move(mouvement);
+    }
     @Override
     public void collidedWith(IMovable other) {
         //TODO
