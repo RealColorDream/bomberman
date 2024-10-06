857b43e1db1a1fc932a7fbb2483ee4b51b550c3b
A. Jakubiak <contact@jakubiak.fr>
2024-10-06 17:08:27 +0200

    :sparkles: :construction: #10 added constructor

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Bomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Bomb.java
@@ -1,8 +1,23 @@
 package fr.univartois.butinfo.r304.bomberman.model.movables;
 
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Bomb extends AbstractMovable {
+
+
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Bomb(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
 
     @Override
     public void collidedWith(IMovable other) {
