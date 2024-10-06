fcc67f49fd9944184b10186d20811158be7b622c
A. Jakubiak <contact@jakubiak.fr>
2024-10-06 17:07:27 +0200

    :sparkles: :construction: #10 created class

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Bomb.java
@@ -0,0 +1,21 @@
+package fr.univartois.butinfo.r304.bomberman.model.movables;
+
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+
+public class Bomb extends AbstractMovable {
+
+    @Override
+    public void collidedWith(IMovable other) {
+
+    }
+
+    @Override
+    public void explode() {
+
+    }
+
+    @Override
+    public void hitEnemy() {
+
+    }
+}
