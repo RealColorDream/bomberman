e11333262cbac5eb22890f6107f667a6c99205d5
Emma Grave <graveemma842@gmail.com>
2024-11-08 10:36:41 +0100

    :sparkles: update SpecialBomb

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/SpecialBomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/SpecialBomb.java
@@ -7,13 +7,16 @@ public class SpecialBomb implements BombStrategy {
 
     @Override
     public void explode(Bomb bomb, BombermanGame game) {
-        // Explosion plus petite : affecte moins de cases (moins de cellules)
-        for (int x = -1 * game.getSpriteStore().getSpriteSize(); x <= game.getSpriteStore().getSpriteSize(); x += game.getSpriteStore().getSpriteSize()) {
-            if (Math.abs(x) < 2) { // Affecte uniquement les cases proches
-                game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + x, game.getSpriteStore().getSprite("explosion")));
-                game.addMovable(new Explosion(game, bomb.getX() + x, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
-                game.getCellAt((int) (bomb.getX() + x), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-            }
-        }
+        int spriteSize = game.getSpriteStore().getSpriteSize();
+
+        game.addMovable(new Explosion(game, bomb.getX() + spriteSize, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
+        game.addMovable(new Explosion(game, bomb.getX() - spriteSize, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
+        game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + spriteSize, game.getSpriteStore().getSprite("explosion")));
+        game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() - spriteSize, game.getSpriteStore().getSprite("explosion")));
+
+        game.getCellAt((int) (bomb.getX() + spriteSize), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+        game.getCellAt((int) (bomb.getX() - spriteSize), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+        game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + spriteSize)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+        game.getCellAt((int) bomb.getX(), (int) (bomb.getY() - spriteSize)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
     }
 }
