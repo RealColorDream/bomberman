4fade339e51be70e64d07d999ff023100acdb86b
Emma Grave <graveemma842@gmail.com>
2024-11-08 10:22:18 +0100

    :sparkles: update NormalBomb

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/NormalBomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/NormalBomb.java
@@ -3,15 +3,23 @@ package fr.univartois.butinfo.r304.bomberman.model.movables.bomb;
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.map.Cell;
 
-public class NormalBomb implements BombStrategy{
+public class NormalBomb implements BombStrategy {
+
     @Override
     public void explode(Bomb bomb, BombermanGame game) {
-        // Explosion classique : affecte les cases voisines à une distance de 1 case
-        for (int x = -1 * game.getSpriteStore().getSpriteSize(); x <= game.getSpriteStore().getSpriteSize(); x += 2 * game.getSpriteStore().getSpriteSize()) {
-            game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + x, game.getSpriteStore().getSprite("explosion")));
+        int spriteSize = game.getSpriteStore().getSpriteSize();
+
+        for (int x = -spriteSize; x <= spriteSize; x += spriteSize) {
+
             game.addMovable(new Explosion(game, bomb.getX() + x, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
-            game.getCellAt((int) (bomb.getX() + x), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-            game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + 1)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + x, game.getSpriteStore().getSprite("explosion")));
+
+            if (bomb.getX() + x >= 0 && bomb.getX() + x < game.getWidth()) {
+                game.getCellAt((int) (bomb.getX() + x), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            }
+            if (bomb.getY() + x >= 0 && bomb.getY() + x < game.getHeight()) {  
+                game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + x)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            }
         }
     }
 }
