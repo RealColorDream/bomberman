b2d0a1fc47180026ce6813432845c0d82e406659
Emma Grave <graveemma842@gmail.com>
2024-11-08 11:15:34 +0100

    :sparkles: update Bomb.java

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/Bomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/Bomb.java
@@ -1,15 +1,30 @@
-package fr.univartois.butinfo.r304.bomberman.model.movables;
+package fr.univartois.butinfo.r304.bomberman.model.movables.bomb;
 
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.model.map.Cell;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Bomb extends AbstractMovable {
 
+    private void explodeCellAt(int x, int y) {
+        Cell cellToUpdate = game.getCellAt(x, y);
+        if (cellToUpdate.getWall() != null) {
+            cellToUpdate.getWall().explode();
+            if (cellToUpdate.getWall().getState() == null)
+                cellToUpdate.replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            else {
+                cellToUpdate.getSpriteProperty().set(cellToUpdate.getWall().getSprite());
+            }
+        }
 
+
+    }
 
     long dropTime = -1;
     static final long EXPLODE_DELAY = 5000;
+    long creationTime;
 
     /**
      * Crée une nouvelle instance de AbstractMovable.
@@ -19,24 +34,18 @@ public class Bomb extends AbstractMovable {
      * @param yPosition La position en y initiale de l'objet.
      * @param sprite    L'instance de {@link Sprite} représentant l'objet.
      */
-    protected Bomb(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+    public Bomb(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
         super(game, xPosition, yPosition, sprite);
+        creationTime = System.currentTimeMillis();
     }
 
     public void drop(double x, double y) {
-        // Calculate the tile size
-        int tileSize = game.getSpriteStore().getSpriteSize();
-
-        // Round x and y to the nearest tile position
-        long roundedX = Math.round(x / tileSize) * tileSize;
-        long roundedY = Math.round(y / tileSize) * tileSize;
-
-        // Set the bomb's position
-        xPosition.set(roundedX);
-        yPosition.set(roundedY);
+        xPosition.set(x);
+        yPosition.set(y);
         dropTime = System.currentTimeMillis();
-    }
 
+
+    }
 
     @Override
     public void collidedWith(IMovable other) {
@@ -54,20 +63,18 @@ public class Bomb extends AbstractMovable {
         //Do nothing
     }
 
-    private void addExplosion(double x, double y) {
-        game.addMovable(new Explosion(game, x, y, game.getSpriteStore().getSprite("explosion")));
-    }
-
     @Override
     public boolean move(long delta) {
         if (!isConsumed() && dropTime != -1 && System.currentTimeMillis() >= dropTime + EXPLODE_DELAY) {
             game.addMovable(new Explosion(game, xPosition.get(), yPosition.get(), game.getSpriteStore().getSprite("explosion")));
             for (int x = -1 * game.getSpriteStore().getSpriteSize(); x <= game.getSpriteStore().getSpriteSize(); x += 2 * game.getSpriteStore().getSpriteSize()) {
-                addExplosion(xPosition.get() + x, yPosition.get());
-                addExplosion(xPosition.get(), yPosition.get() + x);
+                game.addMovable(new Explosion(game, xPosition.get() + x, yPosition.get(), game.getSpriteStore().getSprite("explosion")));
+                game.addMovable(new Explosion(game, xPosition.get(), yPosition.get() + x, game.getSpriteStore().getSprite("explosion")));
+
+                explodeCellAt((int) xPosition.get() + x, (int) yPosition.get());
+                explodeCellAt((int) xPosition.get(), (int) (yPosition.get() + x));
             }
             game.removeMovable(this);
-            game.addPlayerBomb();
         }
         return false;
     }
--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/Explosion.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/Explosion.java
@@ -1,8 +1,8 @@
-package fr.univartois.butinfo.r304.bomberman.model.movables;
+package fr.univartois.butinfo.r304.bomberman.model.movables.bomb;
 
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
-import fr.univartois.butinfo.r304.bomberman.model.map.Cell;
+import fr.univartois.butinfo.r304.bomberman.model.movables.AbstractMovable;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Explosion extends AbstractMovable {
@@ -39,23 +39,10 @@ public class Explosion extends AbstractMovable {
     }
 
     @Override
-    public boolean move(long timeDelta) {
-        if (!isConsumed() && System.currentTimeMillis() >= creationTime + EXPLOSION_DELAY) {
-            explodeCellAt((int) xPosition.get(), (int) yPosition.get());
+    public boolean move(long timeDelta){
+        if (!isConsumed() && System.currentTimeMillis() >= creationTime+EXPLOSION_DELAY)
             game.removeMovable(this);
-        }
         return false;
     }
 
-    private void explodeCellAt(int x, int y) {
-        Cell cellToUpdate = game.getCellAt(x, y);
-        if (cellToUpdate.getWall() != null) {
-            cellToUpdate.getWall().explode();
-            if (cellToUpdate.getWall().getState() == null)
-                cellToUpdate.replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-            else {
-                cellToUpdate.getSpriteProperty().set(cellToUpdate.getWall().getSprite());
-            }
-        }
-    }
 }
