198d667350acab5f46d7d183268b6f0806c4ac67
A. Jakubiak <contact@jakubiak.fr>
2024-11-08 18:58:13 +0100

    :boom: Block change state is now handled by explosion

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Bomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Bomb.java
@@ -1,27 +1,12 @@
 package fr.univartois.butinfo.r304.bomberman.model.movables;
 
-import fr.univartois.butinfo.r304.bomberman.Bomberman;
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
 import fr.univartois.butinfo.r304.bomberman.model.IMovable;
-import fr.univartois.butinfo.r304.bomberman.model.map.Cell;
-import fr.univartois.butinfo.r304.bomberman.model.map.wall.states.Brick;
 import fr.univartois.butinfo.r304.bomberman.view.Sprite;
 
 public class Bomb extends AbstractMovable {
 
-    private void explodeCellAt(int x, int y) {
-        Cell cellToUpdate = game.getCellAt(x, y);
-        if (cellToUpdate.getWall() != null) {
-            cellToUpdate.getWall().explode();
-            if (cellToUpdate.getWall().getState() == null)
-                cellToUpdate.replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-            else {
-                cellToUpdate.getSpriteProperty().set(cellToUpdate.getWall().getSprite());
-            }
-        }
 
-
-    }
 
     long dropTime = -1;
     static final long EXPLODE_DELAY = 5000;
@@ -40,11 +25,11 @@ public class Bomb extends AbstractMovable {
 
     public void drop(double x, double y) {
         // Calculate the tile size
-        int tileSize = 32 * Bomberman.SCALE;
+        int tileSize = game.getSpriteStore().getSpriteSize();
 
         // Round x and y to the nearest tile position
-        double roundedX = Math.round(x / tileSize) * tileSize;
-        double roundedY = Math.round(y / tileSize) * tileSize;
+        long roundedX = Math.round(x / tileSize) * tileSize;
+        long roundedY = Math.round(y / tileSize) * tileSize;
 
         // Set the bomb's position
         xPosition.set(roundedX);
@@ -69,16 +54,17 @@ public class Bomb extends AbstractMovable {
         //Do nothing
     }
 
+    private void addExplosion(double x, double y) {
+        game.addMovable(new Explosion(game, x, y, game.getSpriteStore().getSprite("explosion")));
+    }
+
     @Override
     public boolean move(long delta) {
         if (!isConsumed() && dropTime != -1 && System.currentTimeMillis() >= dropTime + EXPLODE_DELAY) {
             game.addMovable(new Explosion(game, xPosition.get(), yPosition.get(), game.getSpriteStore().getSprite("explosion")));
             for (int x = -1 * game.getSpriteStore().getSpriteSize(); x <= game.getSpriteStore().getSpriteSize(); x += 2 * game.getSpriteStore().getSpriteSize()) {
-                game.addMovable(new Explosion(game, xPosition.get() + x, yPosition.get(), game.getSpriteStore().getSprite("explosion")));
-                game.addMovable(new Explosion(game, xPosition.get(), yPosition.get() + x, game.getSpriteStore().getSprite("explosion")));
-
-                explodeCellAt((int) xPosition.get() + x, (int) yPosition.get());
-                explodeCellAt((int) xPosition.get(), (int) (yPosition.get() + x));
+                addExplosion(xPosition.get() + x, yPosition.get());
+                addExplosion(xPosition.get(), yPosition.get() + x);
             }
             game.removeMovable(this);
         }
