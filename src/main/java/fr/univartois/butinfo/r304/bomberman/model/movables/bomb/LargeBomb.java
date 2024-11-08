7b62aec63724274a02d0903860d2f9bdd5f674f1
Emma Grave <graveemma842@gmail.com>
2024-11-08 10:30:22 +0100

    :sparkles: update LargeBomb

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/LargeBomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/LargeBomb.java
@@ -7,12 +7,17 @@ public class LargeBomb implements BombStrategy {
 
     @Override
     public void explode(Bomb bomb, BombermanGame game) {
-        // Explosion plus large : affecte les cases voisines à une distance de 2 cases
-        for (int x = -2 * game.getSpriteStore().getSpriteSize(); x <= 2 * game.getSpriteStore().getSpriteSize(); x += game.getSpriteStore().getSpriteSize()) {
-            game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + x, game.getSpriteStore().getSprite("explosion")));
-            game.addMovable(new Explosion(game, bomb.getX() + x, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
-            game.getCellAt((int) (bomb.getX() + x), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-            game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + x)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+        int spriteSize = game.getSpriteStore().getSpriteSize();
+
+        for (int x = -2 * spriteSize; x <= 2 * spriteSize; x += spriteSize) {
+            if (bomb.getX() + x >= 0 && bomb.getX() + x < game.getWidth())  {
+                game.addMovable(new Explosion(game, bomb.getX() + x, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
+                game.getCellAt((int) (bomb.getX() + x), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            }
+            if (bomb.getY() + x >= 0 && bomb.getY() + x < game.getHeight())   {
+                game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + x, game.getSpriteStore().getSprite("explosion")));
+                game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + x)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+            }
         }
     }
 }
