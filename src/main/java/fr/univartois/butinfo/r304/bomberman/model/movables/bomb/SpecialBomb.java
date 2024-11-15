117d6d4d8a87bd8dd8e190881f8a45220f88b1cc
Emma Grave <graveemma842@gmail.com>
2024-11-15 09:53:35 +0100

    :sparkles: update SpecialBomb

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/SpecialBomb.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/bomb/SpecialBomb.java
@@ -1,22 +1,11 @@
 package fr.univartois.butinfo.r304.bomberman.model.movables.bomb;
 
 import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
-import fr.univartois.butinfo.r304.bomberman.model.map.Cell;
 
 public class SpecialBomb implements BombStrategy {
 
     @Override
     public void explode(Bomb bomb, BombermanGame game) {
-        int spriteSize = game.getSpriteStore().getSpriteSize();
-
-        game.addMovable(new Explosion(game, bomb.getX() + spriteSize, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
-        game.addMovable(new Explosion(game, bomb.getX() - spriteSize, bomb.getY(), game.getSpriteStore().getSprite("explosion")));
-        game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() + spriteSize, game.getSpriteStore().getSprite("explosion")));
-        game.addMovable(new Explosion(game, bomb.getX(), bomb.getY() - spriteSize, game.getSpriteStore().getSprite("explosion")));
-
-        game.getCellAt((int) (bomb.getX() + spriteSize), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-        game.getCellAt((int) (bomb.getX() - spriteSize), (int) bomb.getY()).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-        game.getCellAt((int) bomb.getX(), (int) (bomb.getY() + spriteSize)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
-        game.getCellAt((int) bomb.getX(), (int) (bomb.getY() - spriteSize)).replaceBy(new Cell(game.getSpriteStore().getSprite("lawn")));
+        //make a bomb that explodes the entire line towards which the player is facing
     }
 }
