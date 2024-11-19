c5b4b18866393f36620bf16ba35fa60ee289a4c9
Romain Wallon <romain.wallon@univ-artois.fr>
2024-11-19 22:23:51 +0100

    :bug: Corrige un problème empêchant de générer de nouvelles cartes.

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/BombermanGame.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/BombermanGame.java
@@ -175,6 +175,7 @@ public final class BombermanGame {
      * Démarre la partie de Bomberman.
      */
     public void start() {
+        prepare();
         createMovables();
         initStatistics();
         animation.start();
