a5a9e9d9fa99c3b39549817767d6de8edaa92a54
Romain Wallon <romain.wallon@univ-artois.fr>
2024-11-19 22:41:40 +0100

    :bug: Corrige une duplication d'objets.

--- src/main/java/fr/univartois/butinfo/r304/bomberman/controller/BombermanController.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/controller/BombermanController.java
@@ -132,6 +132,7 @@ public final class BombermanController implements IBombermanController {
      * @param map La carte du jeu à afficher.
      */
     private void createBackground(GameMap map) {
+        backgroundPane.getChildren().clear();
         for (int row = 0; row < map.getHeight(); row++) {
             for (int column = 0; column < map.getWidth(); column++) {
                 Cell cell = map.getAt(row, column);
